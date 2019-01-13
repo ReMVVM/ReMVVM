3673336491ffb0a82b64bb6f1fe6b1681ee1dcb5
Dariusz Grzeszczak <d.grzeszczak@gmail.com>
2019-01-13 22:18:46 +0100

    - carthage build directory added for example

---
+++ ReMVVMBasicExample/Carthage/Build/iOS/SwiftyRedux.framework/Headers/SwiftyRedux.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRedux.h
+//  SwiftyRedux
+//
+//  Created by Dariusz Grzeszczak on 01/10/2018.
+//  Copyright © 2018 Dariusz Grzeszczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyRedux.
+FOUNDATION_EXPORT double SwiftyReduxVersionNumber;
+
+//! Project version string for SwiftyRedux.
+FOUNDATION_EXPORT const unsigned char SwiftyReduxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRedux/PublicHeader.h>
+
+
